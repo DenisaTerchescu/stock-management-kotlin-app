d81e11997e17f8894e5b2089f71b98949cbe11f1
DenisaTerchescu <Denisa@DESKTOP-FTJGMRG>
2025-07-29 23:55:19 +0300

    Added the supplier list viewmodel

---
+++ app/src/main/java/com/example/stockmanagementapp/presentation/SupplierListViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.stockmanagementapp.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.stockmanagementapp.data.model.Supplier
+import com.example.stockmanagementapp.repository.StockRepository
+import com.example.stockmanagementapp.view.navigator.Navigator
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+data class SupplierListState(
+    val supplierList: List<Supplier> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
+
+sealed class SupplierListAction {
+    data object FetchSuppliers : SupplierListAction()
+
+    data class NavigateToSupplierDetailScreen(val productId: Int) : SupplierListAction()
+}
+
+@HiltViewModel
+class SupplierListViewModel @Inject constructor(
+    private val repository: StockRepository, private val navigator: Navigator
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SupplierListState(isLoading = true))
+    val uiState: StateFlow<SupplierListState> = _uiState
+
+    fun onAction(action: SupplierListAction) {
+        when (action) {
+
+            SupplierListAction.FetchSuppliers -> {
+                fetchSuppliers()
+            }
+
+            is SupplierListAction.NavigateToSupplierDetailScreen -> {}
+        }
+    }
+
+    private fun fetchSuppliers() {
+        viewModelScope.launch {
+            repository.getAllSuppliers().catch {
+                    _uiState.value = _uiState.value.copy(error = it.message)
+                }.collect {
+                    _uiState.value = _uiState.value.copy(supplierList = it)
+                }
+
+        }
+    }
+
+}
